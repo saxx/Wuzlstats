9419a1f260d5121cb3c8325ccbed5f646b994f7e
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2015-06-23 13:02:01 +0200

    Added new "hidden" menu bar for league operations.

---
+++ src/Wuzlstats/wwwroot/js/app.leagueMenuBar.js
@@ -0,0 +1,15 @@
+﻿(function (app, $) {
+    app.registerLeagueMenuBar = function () {
+        var menuBar = $('#leagueMenuBar');
+
+        $(document).keydown(function(e) {
+            if (e.ctrlKey) {
+                menuBar.toggle();
+            }
+        });
+
+        $("#scoreBox").dblclick(function() {
+            menuBar.toggle();
+        });
+    }
+}(window.app = window.app || {}, jQuery));
