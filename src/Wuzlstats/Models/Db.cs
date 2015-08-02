d63fd2b4ea73beb70ab35653bfdcdf10fe695bff
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2015-08-02 12:56:54 +0200

    Use CDN instead of local copies of common JS libs.

--- src/Wuzlstats/Models/Db.cs
+++ src/Wuzlstats/Models/Db.cs
@@ -12,8 +12,6 @@ namespace Wuzlstats.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.ForSqlServer().UseIdentity();
-
             modelBuilder.Entity<Game>().Reference(x => x.League).InverseCollection(x => x.Games).ForeignKey(x => x.LeagueId);
             modelBuilder.Entity<Player>().Reference(x => x.League).InverseCollection(x => x.Players).ForeignKey(x => x.LeagueId);
             modelBuilder.Entity<PlayerPosition>().Reference(x => x.Player).InverseCollection(x => x.Positions).ForeignKey(x => x.PlayerId);
