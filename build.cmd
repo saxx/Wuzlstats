e48ffa5e511b2699ef95c1d9805fbb107269dba1
Hannes Sachsenhofer <Hannes@sachsenhofer.com>
2015-08-18 09:30:28 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -2,4 +2,4 @@ IF EXIST %~dp0artifacts\publish rmdir /S /Q %~dp0artifacts\publish
 
 dnu.cmd publish "%~dp0src\Wuzlstats" --out "%~dp0artifacts\publish" --configuration Release --no-source --wwwroot-out "wwwroot"
 
-
+exit 0
