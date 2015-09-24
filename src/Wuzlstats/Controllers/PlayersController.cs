6451060eeb7f0f445621a0b71b21085a829301c4
afuersch <adrian.fuerschuss@gmx.net>
2015-09-24 19:16:04 +0200

    #1 Add action, model and view for simple players list.

---
+++ src/Wuzlstats/Controllers/PlayersController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Wuzlstats.Models;
+using Wuzlstats.ViewModels.Players;
+using System;
+
+namespace Wuzlstats.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private readonly Db _db;
+        private readonly AppSettings _settings;
+
+        public PlayersController(Db db, AppSettings settings)
+        {
+            _settings = settings;
+            _db = db;
+        }
+
+        [Route("~/League/{league}/Players")]
+        public IActionResult Index(string league)
+        {
+            var leagueEntity = _db.Leagues.FirstOrDefault(x => x.Name.ToLower() == league.ToLower());
+            if (leagueEntity == null)
+            {
+                return RedirectToAction("Index", "Leagues");
+            }
+            ViewBag.CurrentLeague = leagueEntity.Name;
+            var players = _db.Players.Where(x => x.LeagueId == leagueEntity.Id).Select(x => new
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Image = x.Image
+            }).ToList();
+
+            return View(players.Select(player => new IndexViewModel
+            {
+                PlayerId = player.Id,
+                Name = player.Name,
+                Image = player.Image == null || player.Image.Length <= 0 ? EmptyAvatar.Base64 : Convert.ToBase64String(player.Image)
+            }));
+        }
+    }
+}
---
+++ src/Wuzlstats/ViewModels/Players/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wuzlstats.ViewModels.Players
+{
+    public class IndexViewModel
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public DateTime LastGamePlayedOn { get; set; }
+    }
+}
