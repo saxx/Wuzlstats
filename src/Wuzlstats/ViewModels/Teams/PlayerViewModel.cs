c13a9e782e6ce0dd2a103f4c32a1fe7f5fcdf836
Adrian Fürschuß <adrian.fuerschuss@epunkt.net>
2016-11-23 18:31:33 +0100

    #5 Add view models for teams.

---
+++ src/Wuzlstats/ViewModels/Teams/PlayerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Wuzlstats.ViewModels.Teams
+{
+    public class PlayerViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ src/Wuzlstats/ViewModels/Teams/TeamViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Wuzlstats.ViewModels.Teams
+{
+    public class TeamViewModel
+    {
+        public PlayerViewModel Player1 { get; set; }
+        public PlayerViewModel Player2 { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public DateTime LastGamePlayedOn { get; set; }
+        public int GamesCount { get; set; }
+
+        // ReSharper disable once PossibleLossOfFraction
+        public double Rank => Losses == 0 ? Wins : (Wins == 0 ? 0.1d / Losses : (double)Wins / Losses);
+
+        public bool Equals(PlayerViewModel p1, PlayerViewModel p2)
+        {
+            if (Player1 == null || Player2 == null)
+            {
+                return false;
+            }
+            if (p1.Id <= p2.Id)
+            {
+                return Player1.Id == p1.Id && Player2.Id == p2.Id;
+            }
+            return Player2.Id == p1.Id && Player1.Id == p2.Id;
+        }
+    }
+}
